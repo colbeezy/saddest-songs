b4b7f132b003dceee69a53357abf74c058abd933
Branden <branden.h.sanders@vanderbilt.edu>
2018-03-18 21:40:47 -0400

    Connect to Spotify API with a test function

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return render_template('message.html', message="Hello bran!")
+
+
+@app.route("/")
+def test():
+    results = sp.search(q='eminem', limit=20)
+    enumeratedResults = list(enumerate(results['tracks']['items']))
+    return render_template('message.html', enumeratedResults=enumeratedResults)
 
 if __name__ == "__main__":
     app.run()
