2818a479758cd1d100106efa961c79b3a5117ac4
Branden <branden.h.sanders@vanderbilt.edu>
2018-03-20 21:40:42 -0400

    Sort tracks by valence

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
 
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -9,11 +10,40 @@ app = Flask(__name__)
 
 
 
-@app.route("/")
-def test():
-    results = sp.search(q='eminem', limit=20)
-    enumeratedResults = list(enumerate(results['tracks']['items']))
-    return render_template('message.html', enumeratedResults=enumeratedResults)
+@app.route("/", methods=["GET", "POST"])
+def artist_lookup():
+    """Lookup artist"""
+
+    # if user reached route via POST
+    if request.method == "POST":
+
+        # Ensure artist was submitted
+        if not request.form.get("artist"):
+            return render_template("error.html")
+
+        artist = request.form.get("artist")
+        if not artist:
+            return render_template("error.html")
+        else:
+            results = sp.search(q=artist, limit=50)
+            enumeratedResults = list(enumerate(results['tracks']['items']))
+            track_ids = []
+            track_names = []
+            for i, t in enumeratedResults:
+                track_ids.append(t['uri'])
+                track_names.append(t['name'])
+            features = sp.audio_features(track_ids)
+            track_valences = []
+            for feature in features:
+                track_valences.append(feature['valence'])
+            namesAndVales = sorted(zip(track_names, track_valences), key=lambda x: x[1])
+            return render_template('results.html', namesAndVales=namesAndVales)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("artist-lookup.html")
+
+
 
 if __name__ == "__main__":
     app.run()
